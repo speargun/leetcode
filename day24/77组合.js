43a2be33a77f66c6c357e6f55d0171644157d3b6
spear <hhzjack@qq.com>
2023-04-28 17:31:07 +0800

    day24 basic complete, day25 initialized

--- day24/77组合.js
+++ day24/77组合.js
@@ -32,5 +32,19 @@
  * @return {number[][]}
  */
 var combine = function (n, k) {
-
+    let res = [];
+    let path = [];
+    function backtracking(n, k, startIndex) {
+        if (path.length == k) {
+            res.push([...path]);
+            return;
+        }
+        for (let i = startIndex; i <= n - (k - path.length) + 1; i++) {
+            path.push(i);
+            backtracking(n, k, i + 1);
+            path.pop();
+        }
+    }
+    backtracking(n, k, 1);
+    return res;
 };
